87e49d34cd50c92ddd728d28572e3d9e2ff23ab9
Seunghun Shin <18shshin@gmail.com>
2024-09-03 14:05:44 +0900

    ✨ Feat: music list view FE part 추가

---
+++ frontend-v1-app/src/components/musicListView.tsx
@@ -0,0 +1,190 @@
+// Todo: AppLayout 수정 필요
+import { use, useEffect, useRef, useState } from "react";
+import * as F from "./musicListView.styled";
+import Axios from "axios";
+const musicListView = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [money, setMoney] = useState("");
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken");
+    // document.cookie = `token=${token}`;
+    async function fn() {
+      try {
+        const data = {
+          access_token: token,
+        };
+        const response = await Axios.post(
+          "http://localhost:8000/user/profile/",
+          data
+        );
+        console.log(response);
+        const {
+          data: { username, password, nickname },
+        } = response;
+
+        setUsername(username);
+        setPassword(password);
+        setNickname(nickname);
+
+        // setEmail(email);
+        // setUsername(userName);
+        // setCampus(campus);
+      } catch (error) {
+        alert(error);
+      }
+    }
+    // fn();
+  }, []);
+
+  const handleSubmit = () => {
+    const token = localStorage.getItem("jwtToken");
+    async function fn() {
+      const data = {
+        access_token: token,
+        money_amount: money,
+      };
+      // alert("asdf");
+      // alert(`${process.env.NEXT_PUBLIC_PAYMENTS_BACKEND_URL_PREFIX}/charge/`);
+      try {
+        const response = await Axios.post(
+          // "http://localhost:8000/user/update_profile/",
+          // "http://localhost:8001/payments/charge",
+          // "/api/payments/charge",
+          `${process.env.NEXT_PUBLIC_PAYMENTS_BACKEND_URL_PREFIX}/charge/`,
+          data
+        );
+        alert("정상적으로 충전되었습니다.");
+      } catch (error) {
+        alert(error);
+      }
+    }
+    fn();
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "money") {
+      setMoney(value);
+    }
+  };
+
+  return (
+    <F.AuthWrapper desc="음원 다운로드를 위한 머니 충전하기">
+      <F.verticalWrapper>
+        {/* <F.InputWithLabel
+          label="충전금액"
+          // type="password"
+          name="money"
+          // value={password}
+          placeholder="충전 금액을 입력 해주세요.   ex) 5000"
+          onChange={handleChange}
+        /> */}
+        {/* <div>
+          <ol>
+            <li draggable="true">Item 1</li>
+            <li draggable="true">Item 1</li>
+            <li draggable="true">Item 1</li>
+          </ol>
+        </div> */}
+        <F.ListViewWrapper>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+          <F.MusicElement
+            imgUrl="/img.png"
+            title="Back To Me"
+            artist="Kanye West"
+            label="test"
+          ></F.MusicElement>
+        </F.ListViewWrapper>
+      </F.verticalWrapper>
+
+      {/* <F.AuthButton onClick={handleSubmit}>충전하기</F.AuthButton> */}
+    </F.AuthWrapper>
+  );
+};
+
+export default musicListView;
