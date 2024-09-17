5484e1dee483e19055fb4fba8c4530e9f232b2ca
Seunghun Shin <18shshin@gmail.com>
2024-09-17 15:31:58 +0900

    ⚡️ Perf: storage 레벨에서 streaming 으로 response 반환하도록 변경

--- audio-storage-backend-app/main.py
+++ audio-storage-backend-app/main.py
@@ -1,18 +1,19 @@
 # audio_storage_backend.py
-import os
 import base64
-from google.cloud import storage
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import Response
-from utils import get_settings
+import os
 from urllib.parse import unquote
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import Response, StreamingResponse
+from google.cloud import storage
+from utils import get_settings
 
 app = FastAPI()
 
 settings = get_settings()
 env = os.getenv("ENVIRONMENT", "dev")
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./graduation-work-434713-2fc06a1d16e0.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./graduation-work-434713-2fc06a1d16e0.json"
 storage_client = storage.Client()
 
 
@@ -22,17 +23,20 @@ def parse_image(img_uri: str):
 
     return image_name
 
+
 def parse_bucket(music_uri: str):
     content = music_uri.split("googleapis.com/")[-1]
     bucket_name = content.split("/")[0]
 
     return bucket_name
 
+
 def parse_music(music_uri: str):
     content = music_uri.split("googleapis.com/")[-1]
     music_name = content.split("/")[-1]
 
     return music_name
+
 
 @app.get("/audio-storage/get_music/{music_uri:path}")
 async def get_music(music_uri: str):
@@ -48,16 +52,12 @@ async def get_music(music_uri: str):
 
         # 파일 내용을 바이트로 다운로드
         content = blob.download_as_bytes()
-        
+
         # 파일 MIME 타입 설정 (여기서는 예시로 'audio/mpeg'를 사용)
         media_type = "audio/mpeg"
-        
-        return Response(
-            content=content,
-            media_type=media_type,
-            headers={"Content-Disposition": f"attachment; filename={music_name}"}
-        )
-            
+
+        return Response(content=content, media_type=media_type, headers={"Content-Disposition": f"attachment; filename={music_name}"})
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
 
@@ -70,21 +70,17 @@ async def get_image(thumbnail_uri: str):
         image_name = parse_image(thumbnail_uri)
         bucket = storage_client.bucket(bucket_name)
         blob = bucket.blob(f"thumbnail/{image_name}")
-        
+
         if not blob.exists():
             raise HTTPException(status_code=404, detail="File not found")
 
         # 파일 내용을 바이트로 다운로드
         content = blob.download_as_bytes()
         # 파일 MIME 타입 설정 (여기서는 예시로 'image/jpg'를 사용)
-        
+
         media_type = "image/jpeg"
-        
-        return Response(
-            content=content,
-            media_type=media_type,
-            headers={"Content-Disposition": f"attachment; filename={image_name}"}
-        )
-            
+
+        return StreamingResponse(content=iter([content]), media_type=media_type, headers={"Content-Disposition": f"attachment; filename={image_name}"})
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
