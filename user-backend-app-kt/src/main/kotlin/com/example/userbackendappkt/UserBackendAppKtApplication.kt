32000fde3ec869d3702e2e2da5c701f4c483af1e
Seunghun Shin <18shshin@gmail.com>
2024-10-05 17:28:32 +0900

    ✨ Feat: user 서비스 마이그레이션 프로젝트 초기화

---
+++ user-backend-app-kt/src/main/kotlin/com/example/userbackendappkt/UserBackendAppKtApplication.kt
@@ -0,0 +1,12 @@
+package com.example.userbackendappkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+
+@SpringBootApplication
+class UserBackendAppKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<UserBackendAppKtApplication>(*args)
+}
---
+++ user-backend-app-kt/src/test/kotlin/com/example/userbackendappkt/UserBackendAppKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.userbackendappkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserBackendAppKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
