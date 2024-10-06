0d871fcb6d112d1fe70ff0205c869cd4cb8a1ee7
Seunghun Shin <18shshin@gmail.com>
2024-10-06 09:50:29 +0900

    🔨 Refactor: idea 설정 변경

--- servers/user-backend-app-kt/build.gradle.kts
+++ servers/user-backend-app-kt/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(kotlin("stdlib"))
 }
 
 kotlin {
